ee852505b58cf772360ea72e13bbf6a02eb7aaba
Zheng Junyi <zhengjunyi@live.com>
2026-05-07 21:36:45 +0800

    feat: select the default log level based on the compilation mode
    For release builds, use `info` as the default log level; otherwise, use `debug`.

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,8 @@ pub use server::http_server;
 pub use server::shadow;
 pub use server::webdav as dav;
 pub use server::{ServerConfig, start_server};
+
+#[cfg(debug_assertions)]
+pub const DEFAULT_LOG_LEVEL: &str = "debug";
+#[cfg(not(debug_assertions))]
+pub const DEFAULT_LOG_LEVEL: &str = "info";
